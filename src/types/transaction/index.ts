3fc6d3e2e3efe4841eda52aae98eb4a8e4d4e080
iamthepoe <luca1901p@gmail.com>
2023-10-02 20:56:26 -0300

    delete transactions

--- src/types/transaction/index.ts
+++
@@ -1,9 +0,0 @@
-export type TransactionType = 'deposit' | 'profit' | 'withdraw';
-export type Money = number;
-
-export interface TransactionEntity {
-  id: number;
-  userId: number;
-  type: TransactionType;
-  value: Money;
-}
