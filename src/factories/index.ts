3806a8d946e854eef7efb5ee8bfb5d308fd7edc6
iamthepoe <luca1901p@gmail.com>
2023-10-07 15:46:07 -0300

    feat: create deposit repository factory

--- src/factories/index.ts
+++ src/factories/index.ts
@@ -1,18 +1,23 @@
-import Database from "bun:sqlite"
+import Database from "bun:sqlite";
 import { DatabaseClient } from "../database/database";
 import { UserRepository } from "../repositories/user.repository";
 import { UserService } from "../services/user.service";
+import { DepositRepository } from "../repositories/deposit.repository";
 
-const db = new Database('urubu.db');
+const db = new Database("urubu.db");
 
 export const createDatabaseClient = () => {
   return new DatabaseClient(db);
-}
+};
 
 export const createUserRepository = () => {
   return new UserRepository(db);
-}
+};
 
 export const createUserService = () => {
   return new UserService(createUserRepository());
-}
+};
+
+export const createDepositRepository = () => {
+  return new DepositRepository(db);
+};
